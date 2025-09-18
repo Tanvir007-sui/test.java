5d57d444b9f66bf8bd9228a381aa1ed1a560046b
Tanvir <istieakt@gmail.com>
2025-09-18 20:29:32 +0300

    initial

---
+++ test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // ---------- Hello World ----------
+        System.out.println("Hello, World! Welcome to Java.");
+    }
+}
